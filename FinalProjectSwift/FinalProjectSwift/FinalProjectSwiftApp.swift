313d8bffb832039c7822231ad03f0f9c0d17659a
JuanJose <jhonajose@hotmail.es>
2024-06-25 19:47:32 +0200

    Create branch develop and Xcode project

---
+++ FinalProjectSwift/FinalProjectSwift/FinalProjectSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectSwiftApp.swift
+//  FinalProjectSwift
+//
+//  Created by Juan jose Morales on 25/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
