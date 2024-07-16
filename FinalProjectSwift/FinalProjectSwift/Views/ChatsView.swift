a89ddaf26bec3829108ba55a566f58d7217cf758
JuanJose <jhonajose@hotmail.es>
2024-07-16 13:25:01 +0200

    Code improvements have been made.

--- FinalProjectSwift/FinalProjectSwift/Views/ChatsView.swift
+++ FinalProjectSwift/FinalProjectSwift/Views/ChatsView.swift
@@ -2,19 +2,7 @@
 //  ChatsView.swift
 //  FinalProjectSwift
 //
-//  Created by Juan jose Morales on 4/7/24.
+//  Created by Juan jose Morales on 13/7/24.
 //
 
-import SwiftUI
-
-struct ChatsView: View {
-    var body: some View {
-        Text("Pedro")
-    }
-}
-
-struct ChatsView_Previews: PreviewProvider {
-    static var previews: some View {
-        ChatsView()
-    }
-}
+import Foundation
