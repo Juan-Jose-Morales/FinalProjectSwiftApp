52abab036fc9b124fb3f15c56810f7945a9421fd
Alejandrovga98 <Alejandrovga98@gmail.com>
2024-07-29 13:31:50 +0200

    Analytics

---
+++ FinalProjectSwift/FinalProjectSwift/Managers/AnalyticManager.swift
@@ -0,0 +1,38 @@
+//
+//  AnalyticManager.swift
+//  FinalProjectSwift
+//
+//  Created by Alejandro Vidal Gómez Alves on 29/7/24.
+//
+
+import Foundation
+import FirebaseAnalytics
+import SwiftUI
+
+final class AnalyticManager {
+    private init() {}
+    
+    static let shared = AnalyticManager()
+    public func logEvent(_ event: AnalyticsEvent) {
+        
+        Analytics.logEvent(AnalyticsEventScreenView, parameters: [
+            AnalyticsParameterScreenName: String(describing: type(of: self)),
+            AnalyticsParameterScreenClass: String(describing: type(of: self))
+            ]
+        )
+    }
+    enum AnalyticsEvent {
+        case navigation(NavigationScreenEvent)
+        
+        var event : String {
+            switch self {
+            case .navigation: return "Navigation"
+            }
+        }
+    }
+}
+
+struct NavigationScreenEvent: Codable {
+    let nameScreen: String
+    let timeStamp: Date
+}
